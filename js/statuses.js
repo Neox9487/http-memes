094caa355b2d7d8cc65bdbc55d10788ffca041ed
Neox9487 <neox9487@gmail.com>
2026-01-03 15:40:07 +0800

    feat: status code - 101

--- js/statuses.js
+++ js/statuses.js
@@ -1,7 +1,7 @@
 export const STATUSES = [
     /* 1xx Informational */
     { code: 100, title: "Continue", category: "1xx", description: "請繼續，目前一切正常，伺服器正在等你把話說完", asset: "100" },
-    { code: 101, title: "Switching Protocols", category: "1xx", description: "", asset: "" },
+    { code: 101, title: "Switching Protocols", category: "1xx", description: "協議正在切換中，是時候換個人了", asset: "101" },
     { code: 102, title: "Processing", category: "1xx", description: "", asset: "" },
     { code: 103, title: "Early Hints", category: "1xx", description: "", asset: "" },
 
